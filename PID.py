8ac116973d6e20f0a8a7f70e8b803b29d6f76607
TomNotchYmk <2215776228@qq.com>
2020-11-09 21:05:03 +0800

    task 5 roughly done, in branch PID.

--- PID.py
+++ PID.py
@@ -1,13 +1,35 @@
+import time
+import queue
 class PID:
     output = 0
     def __init__(self, p, i, d):
         self.p = p
         self.i = i
         self.d = d
+        self.lastTime = time.time()
+        self.integration = 0
+        self.integrationBuffer = queue.Queue()
     def update(self, setPoint, actualValue, lastActualValue):
+
+        currentTime = time.time()
+        timeDiff = currentTime - self.lastTime
+
         # calculate output
-        error = actualValue - setPoint
-        pOutput = - self.p * error
-        output = pOutput
+        error = setPoint - actualValue
+        pOutput = self.p * error # need proportion threshold
+
+        self.integration += error * timeDiff
+        self.integrationBuffer.put(error)
+        if (self.integrationBuffer.qsize() == 101):
+            self.integration -= self.integrationBuffer.get() * timeDiff
+        # 100 samples for now
+        iOutput = self.i * self.integration # need integration threshold
+
+        lastError = setPoint - lastActualValue
+        dOutput = self.d * (error - lastError) / timeDiff # need derivative threshold
+
+        self.lastTime = currentTime
+
+        output = pOutput + iOutput + dOutput
         # add integral and differential terms
         return output
